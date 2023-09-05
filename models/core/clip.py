a82a08f77dd4f07b172432094dba5d2364135500
xukechun <kcxu@zju.edu.cn>
2023-09-05 16:04:56 +0800

    update

--- models/core/clip.py
+++ models/core/clip.py
@@ -265,27 +265,6 @@ class VisualTransformer(nn.Module):
         self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
 
     def forward(self, x: torch.Tensor):
-        # # !! Modified !!
-        # batch_size, n_obj = x.shape[:2]
-        # x = x.reshape(-1, *x.shape[2:])
-        # x = self.conv1(x)  # shape = [*, width, n_obj, 1, 1] # shape = [*, width, grid, grid] 
-        # x = x.reshape(batch_size, n_obj, -1)  # shape = [*, n_obj, width] # shape = [*, width, grid ** 2] 
-        
-        # # x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
-        # # x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
-        # x = x + self.positional_embedding.to(x.dtype)[:n_obj]
-        # x = self.ln_pre(x)
-
-        # x = x.permute(1, 0, 2)  # NLD -> LND
-        # x = self.transformer(x) 
-        # x = x.permute(1, 0, 2)  # LND -> NLD
-
-        # # x = self.ln_post(x[:, 0, :])
-        # x = self.ln_post(x)
-
-        # if self.proj is not None:
-        #     x = x @ self.proj
-
         x = self.conv1(x)  # shape = [*, width, grid, grid]
         x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
         x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
